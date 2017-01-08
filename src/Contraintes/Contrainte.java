ae5c6c579d8b0b09461e3f2a08704c43357ff7c0
Fabian SAMSON <fabian.samson@etudiant.mines-nantes.fr>
2017-01-08 19:10:02 +0100

    Création d'un package dédié aux conraintes

---
+++ src/Contraintes/Contrainte.java
@@ -0,0 +1,11 @@
+package Contraintes;
+
+import foot2000.Modele;
+
+/**
+ * @author Fab
+ * L'interface représentant les contraintes
+ */
+public interface Contrainte {
+	public void ajout_contrainte(Modele mod);
+}
--- src/foot2000/Contrainte.java
+++
@@ -1,5 +0,0 @@
-package foot2000;
-
-public interface Contrainte {
-	public void ajout_contrainte(Modele mod);
-}
