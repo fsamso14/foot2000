e5bb2f9b9acf118f7f4a0b40b1c7a916de204760
Fabian SAMSON <fabian.samson@etudiant.mines-nantes.fr>
2017-01-08 18:19:51 +0100

    Création de la classe Contraintes

---
+++ src/foot2000/Contraintes.java
@@ -0,0 +1,29 @@
+package foot2000;
+
+/**
+ * @author Fab
+ * La Classe des contraintes recense l'ensemble des contraintes existantes
+ */
+public class Contraintes {
+	// Le nombre d'Arbitres pour déterminer le domaine de valeurs des variables
+	private int nbArbitres;
+	
+	// Le nombre de matchs pour l'indexage des variables
+	private int nbMatchs;
+	
+	// Les constantes qui correspondent à la position de l'arbitre
+	public final static int ARBITRE_CENTRAL =0;
+	public final static int ARBITRE_TOUCHE1 =1;
+	public final static int ARBITRE_TOUCHE2 =2;
+	
+	/**
+	 * Le Constructeur de la classe contraintes
+	 * @param nbArbitres
+	 * @param nbMatchs
+	 */
+	public Contraintes(int nbArbitres,int nbMatchs){
+		this.nbArbitres = nbArbitres;
+		this.nbMatchs = nbMatchs;
+	}
+
+}
