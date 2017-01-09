6e9f2d82799923954ba69ab523f401f83c0f134e
Fabian SAMSON <fabian.samson@etudiant.mines-nantes.fr>
2017-01-09 13:16:30 +0100

    Transformation en Projet Maven

--- src/Contraintes/ContrainteSurGrade.java
+++ src/Contraintes/ContrainteSurGrade.java
@@ -1,5 +1,13 @@
 package Contraintes;
 
-public class ContrainteSurGrade {
+import foot2000.Modele;
+
+public class ContrainteSurGrade implements Contrainte{
+
+	@Override
+	public void ajout_contrainte(Modele mod) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
