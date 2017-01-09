f8c2ad61b497f99c5a7476f5a503977687c1817c
thaianthantrong <thaianthantrong@mbp-de-thai.eduroam.emn.fr>
2017-01-09 13:56:36 +0100

    Ajout CategorieMatch

---
+++ src/Representation/CategorieMatch.java
@@ -0,0 +1,21 @@
+package Representation;
+
+public class CategorieMatch {
+
+	public final int DSD = 12;
+	public final int D1 = 11;
+	public final int D2 = 10;
+	public final int D3 = 9;
+	public final int D4 = 8;
+	
+	public final int AAD1 = 7;
+	public final int AAD2 = 6;
+	
+	public final int U19R = 5;
+	public final int U18A = 4;
+	public final int U18B = 3;
+	public final int U16 = 2;
+	public final int U15 = 1;
+	
+
+}
